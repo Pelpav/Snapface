d8854bbb6cf0946eae5d27900192338df4fef3fb
Pelpav <elpav2022@gmail.com>
2023-12-07 14:01:12 +0100

    modified:   snapface/src/app/app.component.scss 	modified:   snapface/src/app/app.component.ts 	modified:   snapface/src/app/face-snap/face-snap.component.html 	modified:   snapface/src/app/face-snap/face-snap.component.scss 	modified:   snapface/src/app/face-snap/face-snap.component.ts 	modified:   snapface/src/index.html

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss',
 })
-export class FaceSnapComponent {}
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps!: number;
+  img!: string;
+
+  ngOnInit(): void {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami';
+    this.createDate = new Date();
+    this.snaps = 7;
+    this.img =
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
+
+  onAddSnap() {
+    ++this.snaps;
+  }
+}
